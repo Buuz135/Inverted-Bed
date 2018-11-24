d345afa2c951118f5ca4b1a292c8d84a57aaf010
Buuz135 <buuz135@hotmail.com>
2018-11-24 19:28:21 +0100

    Initial Release

--- src/main/java/com/buuz135/invertedbed/Invertedbed.java
+++ src/main/java/com/buuz135/invertedbed/Invertedbed.java
@@ -1,10 +1,33 @@
+/*
+ * This file is part of Industrial Foregoing.
+ *
+ * Copyright 2018, Buuz135
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.buuz135.invertedbed;
 
 import com.buuz135.invertedbed.block.BlockInvertedBed;
 import com.buuz135.invertedbed.item.ItemInvertedBed;
 import com.buuz135.invertedbed.tile.TileEntityInvertedBed;
+import com.buuz135.invertedbed.tile.TileEntityInvertedBedRenderer;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -19,6 +42,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod(
         modid = Invertedbed.MOD_ID,
@@ -43,14 +67,19 @@ public class Invertedbed {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        if (event.getSide() == Side.CLIENT) {
-            ClientRegistry.bindTileEntitySpecialRenderer(TileEntityInvertedBed.class, new TileEntityInvertedBedRenderer());
-        }
+        if (event.getSide() == Side.CLIENT) registerTESR();
+        OreDictionary.registerOre("blockWool", Blocks.WOOL);
     }
 
     @Mod.EventHandler
     public void postinit(FMLPostInitializationEvent event) {
 
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerTESR() {
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityInvertedBed.class, new TileEntityInvertedBedRenderer());
+        ITEM_INVERTED_BED.registerTESR();
     }
 
     @Mod.EventBusSubscriber
--- src/main/java/com/buuz135/invertedbed/item/ItemInvertedBed.java
+++ src/main/java/com/buuz135/invertedbed/item/ItemInvertedBed.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Industrial Foregoing.
+ *
+ * Copyright 2018, Buuz135
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.buuz135.invertedbed.item;
 
 import com.buuz135.invertedbed.Invertedbed;
@@ -15,6 +36,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemInvertedBed extends Item {
 
@@ -22,7 +45,6 @@ public class ItemInvertedBed extends Item {
         this.setCreativeTab(CreativeTabs.DECORATIONS);
         this.setRegistryName(Invertedbed.MOD_ID, "bed_item");
         this.setTranslationKey("invertedbed.bed");
-        setTileEntityItemStackRenderer(new ItemInvertedBedTESR());
     }
 
     @Override
@@ -74,4 +96,10 @@ public class ItemInvertedBed extends Item {
 
         }
     }
+
+    @SideOnly(Side.CLIENT)
+    public void registerTESR() {
+        setTileEntityItemStackRenderer(ItemInvertedBedTESR.INSTANCE);
+    }
+
 }
